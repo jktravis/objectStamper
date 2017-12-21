91b28e63972c6f24310cee845f1906f932d7df6e
jktravis <joshua.travis@gmail.com>
2017-12-20 21:49:18 -0500

    App implementation.

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-const { objectStamper } = require('objectStamper');
+const { objectStamper } = require('./objectStamper');
+
+const fooStamp = objectStamper({
+  props: {
+    Foo: 'foo'
+  },
+  init({ Foo } = {}) {
+    this.Foo = Foo ? Foo : this.Foo;
+  },
+  methods: {
+    sayFoo() {
+      console.log(this.Foo);
+    }
+  }
+});
+
+const firstFoo = fooStamp();
+firstFoo.sayFoo();
+
+const secondFoo = fooStamp({ Foo: 'Fooey oey oo' });
+secondFoo.sayFoo();
+
+console.log('same foo? ', firstFoo === secondFoo);
+
+const barStamp = objectStamper({
+  props: {
+    Bar: 'bar'
+  },
+  init({ Bar } = {}) {
+    this.Bar = Bar ? Bar : this.Bar;
+  },
+  methods: {
+    sayBar() {
+      console.log(this.Bar);
+    }
+  }
+});
+
+const firstBar = barStamp();
+firstBar.sayBar();
+
+const secondBar = barStamp({ Bar: 'Bar-e-ar-e-ar' });
+secondBar.sayBar();
+
+console.log('same bar? ', firstBar === secondBar);
